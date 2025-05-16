366ff5fc322632d68ef5d211fec389f28048582e
Milligann8 <milligann8@gmail.com>
2025-05-16 16:36:45 -0600

    Update oobetasks.ps1

--- ScriptPad/oobetasks.ps1
+++ ScriptPad/oobetasks.ps1
@@ -1,19 +1,16 @@
-# oobe.osdcloud.ch - needs to be here for blog post on akosbakos.ch
-
 [CmdletBinding()]
 param()
 #=================================================
 #Script Information
-
 #=================================================
 #region Initialize
 
 #Start the Transcript
-$Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-OSDCloud.log"
+$Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-SystemTasks.log"
 $null = Start-Transcript -Path (Join-Path "$env:SystemRoot\Temp" $Transcript) -ErrorAction Ignore
 
 #=================================================
-#   oobeCloud Settings
+#    oobeCloud Settings
 #=================================================
 $Global:oobeCloud = @{
     oobeSetDisplay = $true
@@ -28,24 +25,23 @@ $Global:oobeCloud = @{
 }
 
 function Step-KeyboardLanguage {
-
     Write-Host -ForegroundColor Green "Set keyboard language to en-US"
     Start-Sleep -Seconds 5
-    
+
     $LanguageList = Get-WinUserLanguageList
-    
+
     $LanguageList.Add("en-US")
     Set-WinUserLanguageList $LanguageList -Force
-    
+
     Start-Sleep -Seconds 5
-    
+
     $LanguageList = Get-WinUserLanguageList
     Set-WinUserLanguageList $LanguageList -Force | Out-Null
 }
 function Step-oobeSetDisplay {
     [CmdletBinding()]
     param ()
-    if (($env:UserName -eq 'defaultuser0') -and ($Global:oobeCloud.oobeSetDisplay -eq $true)) {
+    if ($Global:oobeCloud.oobeSetDisplay -eq $true) {
         Write-Host -ForegroundColor Yellow 'Verify the Display Resolution and Scale is set properly'
         Start-Process 'ms-settings:display' | Out-Null
         $ProcessId = (Get-Process -Name 'SystemSettings').Id
@@ -57,7 +53,7 @@ function Step-oobeSetDisplay {
 function Step-oobeSetRegionLanguage {
     [CmdletBinding()]
     param ()
-    if (($env:UserName -eq 'defaultuser0') -and ($Global:oobeCloud.oobeSetRegionLanguage -eq $true)) {
+    if ($Global:oobeCloud.oobeSetRegionLanguage -eq $true) {
         Write-Host -ForegroundColor Yellow 'Verify the Language, Region, and Keyboard are set properly'
         Start-Process 'ms-settings:regionlanguage' | Out-Null
         $ProcessId = (Get-Process -Name 'SystemSettings').Id
@@ -69,7 +65,7 @@ function Step-oobeSetRegionLanguage {
 function Step-oobeSetDateTime {
     [CmdletBinding()]
     param ()
-    if (($env:UserName -eq 'defaultuser0') -and ($Global:oobeCloud.oobeSetDateTime -eq $true)) {
+    if ($Global:oobeCloud.oobeSetDateTime -eq $true) {
         Write-Host -ForegroundColor Yellow 'Verify the Date and Time is set properly including the Time Zone'
         Write-Host -ForegroundColor Yellow 'If this is not configured properly, Certificates and Domain Join may fail'
         Start-Process 'ms-settings:dateandtime' | Out-Null
@@ -82,49 +78,40 @@ function Step-oobeSetDateTime {
 function Step-oobeExecutionPolicy {
     [CmdletBinding()]
     param ()
-    if ($env:UserName -eq 'defaultuser0') {
-        if ((Get-ExecutionPolicy) -ne 'RemoteSigned') {
-            Write-Host -ForegroundColor Cyan 'Set-ExecutionPolicy RemoteSigned'
-            Set-ExecutionPolicy RemoteSigned -Force
-        }
+    if ((Get-ExecutionPolicy) -ne 'RemoteSigned') {
+        Write-Host -ForegroundColor Cyan 'Set-ExecutionPolicy RemoteSigned'
+        Set-ExecutionPolicy RemoteSigned -Force
     }
 }
 function Step-oobePackageManagement {
     [CmdletBinding()]
     param ()
-    if ($env:UserName -eq 'defaultuser0') {
-        if (Get-Module -Name PowerShellGet -ListAvailable | Where-Object {$_.Version -ge '2.2.5'}) {
-            Write-Host -ForegroundColor Cyan 'PowerShellGet 2.2.5 or greater is installed'
-        }
-        else {
-            Write-Host -ForegroundColor Cyan 'Install-Package PackageManagement,PowerShellGet'
-            Install-Package -Name PowerShellGet -MinimumVersion 2.2.5 -Force -Confirm:$false -Source PSGallery | Out-Null
-    
-            Write-Host -ForegroundColor Cyan 'Import-Module PackageManagement,PowerShellGet'
-            Import-Module PackageManagement,PowerShellGet -Force
-        }
+    if (Get-Module -Name PowerShellGet -ListAvailable | Where-Object {$_.Version -ge '2.2.5'}) {
+        Write-Host -ForegroundColor Cyan 'PowerShellGet 2.2.5 or greater is installed'
+    }
+    else {
+        Write-Host -ForegroundColor Cyan 'Install-Package PackageManagement,PowerShellGet'
+        Install-Package -Name PowerShellGet -MinimumVersion 2.2.5 -Force -Confirm:$false -Source PSGallery | Out-Null
+
+        Write-Host -ForegroundColor Cyan 'Import-Module PackageManagement,PowerShellGet'
+        Import-Module PackageManagement,PowerShellGet -Force
     }
 }
 function Step-oobeTrustPSGallery {
     [CmdletBinding()]
     param ()
-    if ($env:UserName -eq 'defaultuser0') {
-        $PSRepository = Get-PSRepository -Name PSGallery
-        if ($PSRepository)
+    $PSRepository = Get-PSRepository -Name PSGallery
+    if ($PSRepository)
+    {
+        if ($PSRepository.InstallationPolicy -ne 'Trusted')
         {
-            if ($PSRepository.InstallationPolicy -ne 'Trusted')
-            {
-                Write-Host -ForegroundColor Cyan 'Set-PSRepository PSGallery Trusted'
-                Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-            }
+            Write-Host -ForegroundColor Cyan 'Set-PSRepository PSGallery Trusted'
+            Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
         }
     }
 }
-
-
-
 function Step-oobeRemoveAppxPackage {
-    if (($env:UserName -eq 'defaultuser0') -and ($Global:oobeCloud.oobeRemoveAppxPackage -eq $true)) {
+    if ($Global:oobeCloud.oobeRemoveAppxPackage -eq $true) {
         Write-Host -ForegroundColor Cyan 'Removing Appx Packages'
         foreach ($Item in $Global:oobeCloud.oobeRemoveAppxPackageName) {
             if (Get-Command Get-AppxProvisionedPackage) {
@@ -155,11 +142,10 @@ function Step-oobeRemoveAppxPackage {
         }
     }
 }
-
 function Step-oobeUpdateDrivers {
     [CmdletBinding()]
     param ()
-    if (($env:UserName -eq 'defaultuser0') -and ($Global:oobeCloud.oobeUpdateDrivers -eq $true)) {
+    if ($Global:oobeCloud.oobeUpdateDrivers -eq $true) {
         Write-Host -ForegroundColor Cyan 'Updating Windows Drivers'
         if (!(Get-Module PSWindowsUpdate -ListAvailable -ErrorAction Ignore)) {
             try {
@@ -178,7 +164,7 @@ function Step-oobeUpdateDrivers {
 function Step-oobeUpdateWindows {
     [CmdletBinding()]
     param ()
-    if (($env:UserName -eq 'defaultuser0') -and ($Global:oobeCloud.oobeUpdateWindows -eq $true)) {
+    if ($Global:oobeCloud.oobeUpdateWindows -eq $true) {
         Write-Host -ForegroundColor Cyan 'Updating Windows'
         if (!(Get-Module PSWindowsUpdate -ListAvailable)) {
             try {
@@ -190,23 +176,17 @@ function Step-oobeUpdateWindows {
             }
         }
         if (Get-Module PSWindowsUpdate -ListAvailable -ErrorAction Ignore) {
-            #Write-Host -ForegroundColor DarkCyan 'Add-WUServiceManager -MicrosoftUpdate -Confirm:$false'
             Add-WUServiceManager -MicrosoftUpdate -Confirm:$false | Out-Null
-            #Write-Host -ForegroundColor DarkCyan 'Install-WindowsUpdate -UpdateType Software -AcceptAll -IgnoreReboot'
-            #Install-WindowsUpdate -UpdateType Software -AcceptAll -IgnoreReboot -NotTitle 'Malicious'
-            #Write-Host -ForegroundColor DarkCyan 'Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot'
             Start-Process PowerShell.exe -ArgumentList "-Command Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot -NotTitle 'Preview' -NotKBArticleID 'KB890830','KB5005463','KB4481252'" -Wait
         }
     }
 }
-
-
 function Step-oobeRestartComputer {
     [CmdletBinding()]
     param ()
-    if (($env:UserName -eq 'defaultuser0') -and ($Global:oobeCloud.oobeRestartComputer -eq $true)) {
+    if ($Global:oobeCloud.oobeRestartComputer -eq $true) {
         Write-Host -ForegroundColor Cyan 'Build Complete!'
-        Write-Warning 'Device will restart in 30 seconds.  Press Ctrl + C to cancel'
+        Write-Warning 'Device will restart in 30 seconds. Press Ctrl + C to cancel'
         Stop-Transcript
         Start-Sleep -Seconds 30
         Restart-Computer
@@ -215,7 +195,7 @@ function Step-oobeRestartComputer {
 function Step-oobeStopComputer {
     [CmdletBinding()]
     param ()
-    if (($env:UserName -eq 'defaultuser0') -and ($Global:oobeCloud.oobeStopComputer -eq $true)) {
+    if ($Global:oobeCloud.oobeStopComputer -eq $true) {
         Write-Host -ForegroundColor Cyan 'Build Complete!'
         Write-Warning 'Device will shutdown in 30 seconds. Press Ctrl + C to cancel'
         Stop-Transcript
@@ -223,8 +203,6 @@ function Step-oobeStopComputer {
         Stop-Computer
     }
 }
-# C:\Windows\Setup\Scripts\oobe.ps1
-
 function Rename-ComputerWithSerialNumber {
     <#
     .SYNOPSIS
@@ -238,10 +216,8 @@ function Rename-ComputerWithSerialNumber {
     param (
         [Switch]$Restart = $false
     )
-
     # Set PowerShell Execution Policy if needed
     Set-ExecutionPolicy Bypass -Scope LocalMachine -Force -ErrorAction SilentlyContinue
-
     try {
         # Get the Serial Number
         $SerialNumber = Get-WmiObject Win32_BIOS | Select-Object -ExpandProperty SerialNumber
@@ -249,14 +225,11 @@ function Rename-ComputerWithSerialNumber {
             Write-Warning "Could not retrieve serial number. Skipping computer renaming."
             return
         }
-
         # Define the new Computer Name
         $NewComputerName = "arivo-$SerialNumber"
-
         # Rename the Computer
         Write-Host "Renaming computer to '$NewComputerName'..."
         Rename-Computer -NewName $NewComputerName -Force
-
         if ($Restart) {
             Write-Host "Restarting computer to apply the new name..."
             Restart-Computer -Force
@@ -267,15 +240,6 @@ function Rename-ComputerWithSerialNumber {
         Write-Error "An error occurred during computer renaming: $($_.Exception.Message)"
     }
 }
-
-
-# C:\
-
-# Call the function to generate and apply the unattend.xml
-
-
-# Call the function to rename the computer and restart
-#endregion
 
 # Execute functions
 Step-KeyboardLanguage
