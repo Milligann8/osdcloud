8f1bd5fa146dea14f460ed5a8258db523a9f5c3b
Milligann8 <milligann8@gmail.com>
2025-05-15 21:38:10 -0600

    Update oobetasks.ps1

--- ScriptPad/oobetasks.ps1
+++ ScriptPad/oobetasks.ps1
@@ -1,4 +1,4 @@
-# oobetasks.osdcloud.ch
+# oobetasks
 
 $scriptFolderPath = "$env:SystemDrive\OSDCloud\Scripts"
 $ScriptPathOOBE = $(Join-Path -Path $scriptFolderPath -ChildPath "OOBE.ps1")
@@ -9,8 +9,8 @@ If(!(Test-Path -Path $scriptFolderPath)) {
 }
 
 $OOBEScript =@"
-$Global:Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-OOBEScripts.log"
-Start-Transcript -Path (Join-Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD\" $Global:Transcript) -ErrorAction Ignore | Out-Null
+`$Global:Transcript = "`$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-OOBEScripts.log"
+Start-Transcript -Path (Join-Path "`$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD\" `$Global:Transcript) -ErrorAction Ignore | Out-Null
 
 
 Write-Host -ForegroundColor DarkGray "Installing OSD PS Module"
@@ -23,6 +23,7 @@ Write-Host -ForegroundColor DarkGray "Executing Product Key Script"
 Start-Process PowerShell -ArgumentList "-NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/Milligann8/osdcloud/refs/heads/main/ScriptPad/embeddedkey.ps1" -Wait
 
 
+
 Write-Host -ForegroundColor DarkGray "Executing OOBEDeploy Script fomr OSDCloud Module"
 Start-Process PowerShell -ArgumentList "-NoL -C Start-OOBEDeploy" -Wait
 
@@ -31,8 +32,8 @@ Start-Process PowerShell -ArgumentList "-NoL -C Invoke-WebPSScript https://raw.g
 
 # Cleanup scheduled Tasks
 Write-Host -ForegroundColor DarkGray "Unregistering Scheduled Tasks"
-Unregister-ScheduledTask -TaskName "Scheduled Task for SendKeys" -Confirm:$false
-Unregister-ScheduledTask -TaskName "Scheduled Task for OSDCloud post installation" -Confirm:$false
+Unregister-ScheduledTask -TaskName "Scheduled Task for SendKeys" -Confirm:`$false
+Unregister-ScheduledTask -TaskName "Scheduled Task for OSDCloud post installation" -Confirm:`$false
 
 Write-Host -ForegroundColor DarkGray "Restarting Computer"
 Start-Process PowerShell -ArgumentList "-NoL -C Restart-Computer -Force" -Wait
@@ -43,21 +44,21 @@ Stop-Transcript -Verbose | Out-File
 Out-File -FilePath $ScriptPathOOBE -InputObject $OOBEScript -Encoding ascii
 
 $SendKeysScript = @"
-$Global:Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-SendKeys.log"
-Start-Transcript -Path (Join-Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD\" $Global:Transcript) -ErrorAction Ignore | Out-Null
+`$Global:Transcript = "`$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-SendKeys.log"
+Start-Transcript -Path (Join-Path "`$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD\" `$Global:Transcript) -ErrorAction Ignore | Out-Null
 
 Write-Host -ForegroundColor DarkGray "Stop Debug-Mode (SHIFT + F10) with WscriptShell.SendKeys"
-$WscriptShell = New-Object -com Wscript.Shell
+`$WscriptShell = New-Object -com Wscript.Shell
 
 # ALT + TAB
 Write-Host -ForegroundColor DarkGray "SendKeys: ALT + TAB"
-$WscriptShell.SendKeys("%({TAB})")
+`$WscriptShell.SendKeys("%({TAB})")
 
 Start-Sleep -Seconds 1
 
 # Shift + F10
 Write-Host -ForegroundColor DarkGray "SendKeys: SHIFT + F10"
-$WscriptShell.SendKeys("+({F10})")
+`$WscriptShell.SendKeys("+({F10})")
 
 Stop-Transcript -Verbose | Out-File
 "@
