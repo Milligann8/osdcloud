8f4ba080d804f2ad7fcaa2e9911daf439e9106db
Milligann8 <milligann8@gmail.com>
2025-05-19 11:04:05 -0600

    Update oobetasks.ps1

--- ScriptPad/oobetasks.ps1
+++ ScriptPad/oobetasks.ps1
@@ -1,16 +1,13 @@
 [CmdletBinding()]
 param()
-#=================================================
-#Script Information
-#=================================================
 #region Initialize
 
 #Start the Transcript
-$Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-SystemTasks.log"
+$Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-OSDCloud.log"
 $null = Start-Transcript -Path (Join-Path "$env:SystemRoot\Temp" $Transcript) -ErrorAction Ignore
 
 #=================================================
-#    oobeCloud Settings
+#   oobeCloud Settings
 #=================================================
 $Global:oobeCloud = @{
     oobeSetDisplay = $true
@@ -18,6 +15,8 @@ $Global:oobeCloud = @{
     oobeSetDateTime = $true
     oobeRemoveAppxPackage = $true
     oobeRemoveAppxPackageName = 'Microsoft.MicrosoftSolitaireCollection'
+    oobeAddCapability = $false
+    oobeAddCapabilityName = 'GroupPolicy','ServerManager','VolumeActivation'
     oobeUpdateDrivers = $true
     oobeUpdateWindows = $true
     oobeRestartComputer = $true
@@ -25,23 +24,25 @@ $Global:oobeCloud = @{
 }
 
 function Step-KeyboardLanguage {
-    Write-Host -ForegroundColor Green "Set keyboard language to en-US"
+
+    Write-Host -ForegroundColor Green "Set keyboard language to de-CH"
     Start-Sleep -Seconds 5
-
+    
     $LanguageList = Get-WinUserLanguageList
-
-    $LanguageList.Add("en-US")
-    Set-WinUserLanguageList $LanguageList -Force
-
+    
+    $LanguageList.Add("de-CH")
+    Set-WinUserLanguageList $LanguageList -Force | Out-Null
+    
     Start-Sleep -Seconds 5
-
+    
     $LanguageList = Get-WinUserLanguageList
+    $LanguageList.Remove(($LanguageList | Where-Object LanguageTag -like 'en-US'))
     Set-WinUserLanguageList $LanguageList -Force | Out-Null
 }
 function Step-oobeSetDisplay {
     [CmdletBinding()]
     param ()
-    if ($Global:oobeCloud.oobeSetDisplay -eq $true) {
+    if (($env:UserName -eq 'defaultuser0') -and ($Global:oobeCloud.oobeSetDisplay -eq $true)) {
         Write-Host -ForegroundColor Yellow 'Verify the Display Resolution and Scale is set properly'
         Start-Process 'ms-settings:display' | Out-Null
         $ProcessId = (Get-Process -Name 'SystemSettings').Id
@@ -53,7 +54,7 @@ function Step-oobeSetDisplay {
 function Step-oobeSetRegionLanguage {
     [CmdletBinding()]
     param ()
-    if ($Global:oobeCloud.oobeSetRegionLanguage -eq $true) {
+    if (($env:UserName -eq 'defaultuser0') -and ($Global:oobeCloud.oobeSetRegionLanguage -eq $true)) {
         Write-Host -ForegroundColor Yellow 'Verify the Language, Region, and Keyboard are set properly'
         Start-Process 'ms-settings:regionlanguage' | Out-Null
         $ProcessId = (Get-Process -Name 'SystemSettings').Id
@@ -65,7 +66,7 @@ function Step-oobeSetRegionLanguage {
 function Step-oobeSetDateTime {
     [CmdletBinding()]
     param ()
-    if ($Global:oobeCloud.oobeSetDateTime -eq $true) {
+    if (($env:UserName -eq 'defaultuser0') -and ($Global:oobeCloud.oobeSetDateTime -eq $true)) {
         Write-Host -ForegroundColor Yellow 'Verify the Date and Time is set properly including the Time Zone'
         Write-Host -ForegroundColor Yellow 'If this is not configured properly, Certificates and Domain Join may fail'
         Start-Process 'ms-settings:dateandtime' | Out-Null
@@ -78,40 +79,47 @@ function Step-oobeSetDateTime {
 function Step-oobeExecutionPolicy {
     [CmdletBinding()]
     param ()
-    if ((Get-ExecutionPolicy) -ne 'RemoteSigned') {
-        Write-Host -ForegroundColor Cyan 'Set-ExecutionPolicy RemoteSigned'
-        Set-ExecutionPolicy RemoteSigned -Force
+    if ($env:UserName -eq 'defaultuser0') {
+        if ((Get-ExecutionPolicy) -ne 'RemoteSigned') {
+            Write-Host -ForegroundColor Cyan 'Set-ExecutionPolicy RemoteSigned'
+            Set-ExecutionPolicy RemoteSigned -Force
+        }
     }
 }
 function Step-oobePackageManagement {
     [CmdletBinding()]
     param ()
-    if (Get-Module -Name PowerShellGet -ListAvailable | Where-Object {$_.Version -ge '2.2.5'}) {
-        Write-Host -ForegroundColor Cyan 'PowerShellGet 2.2.5 or greater is installed'
-    }
-    else {
-        Write-Host -ForegroundColor Cyan 'Install-Package PackageManagement,PowerShellGet'
-        Install-Package -Name PowerShellGet -MinimumVersion 2.2.5 -Force -Confirm:$false -Source PSGallery | Out-Null
-
-        Write-Host -ForegroundColor Cyan 'Import-Module PackageManagement,PowerShellGet'
-        Import-Module PackageManagement,PowerShellGet -Force
+    if ($env:UserName -eq 'defaultuser0') {
+        if (Get-Module -Name PowerShellGet -ListAvailable | Where-Object {$_.Version -ge '2.2.5'}) {
+            Write-Host -ForegroundColor Cyan 'PowerShellGet 2.2.5 or greater is installed'
+        }
+        else {
+            Write-Host -ForegroundColor Cyan 'Install-Package PackageManagement,PowerShellGet'
+            Install-Package -Name PowerShellGet -MinimumVersion 2.2.5 -Force -Confirm:$false -Source PSGallery | Out-Null
+    
+            Write-Host -ForegroundColor Cyan 'Import-Module PackageManagement,PowerShellGet'
+            Import-Module PackageManagement,PowerShellGet -Force
+        }
     }
 }
 function Step-oobeTrustPSGallery {
     [CmdletBinding()]
     param ()
-    $PSRepository = Get-PSRepository -Name PSGallery
-    if ($PSRepository)
-    {
-        if ($PSRepository.InstallationPolicy -ne 'Trusted')
+    if ($env:UserName -eq 'defaultuser0') {
+        $PSRepository = Get-PSRepository -Name PSGallery
+        if ($PSRepository)
         {
-            Write-Host -ForegroundColor Cyan 'Set-PSRepository PSGallery Trusted'
-            Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+            if ($PSRepository.InstallationPolicy -ne 'Trusted')
+            {
+                Write-Host -ForegroundColor Cyan 'Set-PSRepository PSGallery Trusted'
+                Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+            }
         }
     }
 }
+
 function Step-oobeRemoveAppxPackage {
-    if ($Global:oobeCloud.oobeRemoveAppxPackage -eq $true) {
+    if (($env:UserName -eq 'defaultuser0') -and ($Global:oobeCloud.oobeRemoveAppxPackage -eq $true)) {
         Write-Host -ForegroundColor Cyan 'Removing Appx Packages'
         foreach ($Item in $Global:oobeCloud.oobeRemoveAppxPackageName) {
             if (Get-Command Get-AppxProvisionedPackage) {
@@ -142,10 +150,24 @@ function Step-oobeRemoveAppxPackage {
         }
     }
 }
+function Step-oobeAddCapability {
+    if (($env:UserName -eq 'defaultuser0') -and ($Global:oobeCloud.oobeAddCapability -eq $true)) {
+        Write-Host -ForegroundColor Cyan "Add-WindowsCapability"
+        foreach ($Item in $Global:oobeCloud.oobeAddCapabilityName) {
+            $WindowsCapability = Get-WindowsCapability -Online -Name "*$Item*" -ErrorAction SilentlyContinue | Where-Object {$_.State -ne 'Installed'}
+            if ($WindowsCapability) {
+                foreach ($Capability in $WindowsCapability) {
+                    Write-Host -ForegroundColor DarkGray $Capability.DisplayName
+                    $Capability | Add-WindowsCapability -Online | Out-Null
+                }
+            }
+        }
+    }
+}
 function Step-oobeUpdateDrivers {
     [CmdletBinding()]
     param ()
-    if ($Global:oobeCloud.oobeUpdateDrivers -eq $true) {
+    if (($env:UserName -eq 'defaultuser0') -and ($Global:oobeCloud.oobeUpdateDrivers -eq $true)) {
         Write-Host -ForegroundColor Cyan 'Updating Windows Drivers'
         if (!(Get-Module PSWindowsUpdate -ListAvailable -ErrorAction Ignore)) {
             try {
@@ -164,7 +186,7 @@ function Step-oobeUpdateDrivers {
 function Step-oobeUpdateWindows {
     [CmdletBinding()]
     param ()
-    if ($Global:oobeCloud.oobeUpdateWindows -eq $true) {
+    if (($env:UserName -eq 'defaultuser0') -and ($Global:oobeCloud.oobeUpdateWindows -eq $true)) {
         Write-Host -ForegroundColor Cyan 'Updating Windows'
         if (!(Get-Module PSWindowsUpdate -ListAvailable)) {
             try {
@@ -176,17 +198,22 @@ function Step-oobeUpdateWindows {
             }
         }
         if (Get-Module PSWindowsUpdate -ListAvailable -ErrorAction Ignore) {
+            #Write-Host -ForegroundColor DarkCyan 'Add-WUServiceManager -MicrosoftUpdate -Confirm:$false'
             Add-WUServiceManager -MicrosoftUpdate -Confirm:$false | Out-Null
+            #Write-Host -ForegroundColor DarkCyan 'Install-WindowsUpdate -UpdateType Software -AcceptAll -IgnoreReboot'
+            #Install-WindowsUpdate -UpdateType Software -AcceptAll -IgnoreReboot -NotTitle 'Malicious'
+            #Write-Host -ForegroundColor DarkCyan 'Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot'
             Start-Process PowerShell.exe -ArgumentList "-Command Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot -NotTitle 'Preview' -NotKBArticleID 'KB890830','KB5005463','KB4481252'" -Wait
         }
     }
 }
+
 function Step-oobeRestartComputer {
     [CmdletBinding()]
     param ()
-    if ($Global:oobeCloud.oobeRestartComputer -eq $true) {
+    if (($env:UserName -eq 'defaultuser0') -and ($Global:oobeCloud.oobeRestartComputer -eq $true)) {
         Write-Host -ForegroundColor Cyan 'Build Complete!'
-        Write-Warning 'Device will restart in 30 seconds. Press Ctrl + C to cancel'
+        Write-Warning 'Device will restart in 30 seconds.  Press Ctrl + C to cancel'
         Stop-Transcript
         Start-Sleep -Seconds 30
         Restart-Computer
@@ -195,7 +222,7 @@ function Step-oobeRestartComputer {
 function Step-oobeStopComputer {
     [CmdletBinding()]
     param ()
-    if ($Global:oobeCloud.oobeStopComputer -eq $true) {
+    if (($env:UserName -eq 'defaultuser0') -and ($Global:oobeCloud.oobeStopComputer -eq $true)) {
         Write-Host -ForegroundColor Cyan 'Build Complete!'
         Write-Warning 'Device will shutdown in 30 seconds. Press Ctrl + C to cancel'
         Stop-Transcript
@@ -203,53 +230,18 @@ function Step-oobeStopComputer {
         Stop-Computer
     }
 }
-function Rename-ComputerWithSerialNumber {
-    <#
-    .SYNOPSIS
-        Renames the computer using the prefix "arivo-" followed by the system's serial number.
-        Optionally restarts the computer after renaming.
-    .DESCRIPTION
-        This function retrieves the serial number of the computer using WMI,
-        constructs a new computer name with the "arivo-" prefix, renames the computer,
-        and optionally restarts the system.
-    #>
-    param (
-        [Switch]$Restart = $false
-    )
-    # Set PowerShell Execution Policy if needed
-    Set-ExecutionPolicy Bypass -Scope LocalMachine -Force -ErrorAction SilentlyContinue
-    try {
-        # Get the Serial Number
-        $SerialNumber = Get-WmiObject Win32_BIOS | Select-Object -ExpandProperty SerialNumber
-        if (-not $SerialNumber) {
-            Write-Warning "Could not retrieve serial number. Skipping computer renaming."
-            return
-        }
-        # Define the new Computer Name
-        $NewComputerName = "arivo-$SerialNumber"
-        # Rename the Computer
-        Write-Host "Renaming computer to '$NewComputerName'..."
-        Rename-Computer -NewName $NewComputerName -Force
-        if ($Restart) {
-            Write-Host "Restarting computer to apply the new name..."
-            Restart-Computer -Force
-        } else {
-            Write-Host "Computer renamed successfully. A restart is required for the new name to take effect."
-        }
-    } catch {
-        Write-Error "An error occurred during computer renaming: $($_.Exception.Message)"
-    }
-}
+#endregion
 
 # Execute functions
 Step-KeyboardLanguage
 Step-oobeExecutionPolicy
+Step-oobePackageManagement
 Step-oobeTrustPSGallery
 Step-oobeSetDisplay
 Step-oobeSetRegionLanguage
 Step-oobeSetDateTime
-Step-oobePackageManagement
 Step-oobeRemoveAppxPackage
+Step-oobeAddCapability
 Step-oobeUpdateDrivers
 Step-oobeUpdateWindows
 Step-oobeRestartComputer
